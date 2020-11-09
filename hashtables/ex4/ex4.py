5fe634b8d85308c6ab330d866a60e217b39591fd
Chris Filkins <filchyboy@gmail.com>
2020-11-08 20:18:47 -0800

    Added ex4. Passed 2 tests

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,13 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    table = {}
+    for i in range(len(a)):
+        if (a[i] < 0 and  abs(a[i]) in table) or (a[i] > 0 and -a[i] in table):
+            result.append(abs(a[i]))
+        table[a[i]] = True
 
     return result
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    print(has_negatives([-1, -3, -2, 1, 2, 3, 4, -4]))
