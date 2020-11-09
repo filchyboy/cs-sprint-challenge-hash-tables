dcf86817003c8e833e75dd2f369c15b9a4162ff3
Chris Filkins <filchyboy@gmail.com>
2020-11-08 18:22:26 -0800

    Ran ex1. Passes tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,15 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    if len(weights) < 2:
+        return None
+
+    weight_hash = {}
+
+    for i in range(len(weights)):
+        if limit - weights[i] in weight_hash:
+            return (i, weight_hash[limit - weights[i]])
+        weight_hash[weights[i]] = i
 
     return None
+
